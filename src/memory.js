c506d9ae2ac883593084f23b9fc00f6712246337
bubu-destroi <zzzarolina@gmail.com>
2024-04-18 23:35:13 +0100

    first commit

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,43 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards){return undefined}
+    else {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]]
+      }
+      return this.cards
+  }
   }
 
   checkIfPair(card1, card2) {
+  if(card1 === card2){
+    this.pairsClicked +=1;
+    this.pairsGuessed +=1;
+    return true
+  }
+  else {
+    this.pairsClicked +=1
+    return false
+  }
     // ... write your code here
   }
 
   checkIfFinished() {
+    if (this.pairsGuessed != this.cards.length/2){
+      return false
+    }else
+    return true
+
     // ... write your code here
   }
 }
